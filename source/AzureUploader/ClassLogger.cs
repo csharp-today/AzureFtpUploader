4e3673cc1180a2f90ea4d1207791df853eeefda9
Mariusz Bojkowski <mariusz.bojkowski@gmail.com>
2019-07-19 17:26:25 +0200

    Update logger header

--- source/AzureUploader/ClassLogger.cs
+++ source/AzureUploader/ClassLogger.cs
@@ -4,10 +4,11 @@ namespace AzureUploader
 {
     internal class ClassLogger<T> : IClassLogger
     {
+        private readonly string _className = typeof(T).Name;
         private readonly ILogger _logger;
 
         public ClassLogger(ILogger logger) => _logger = logger;
 
-        public void Log(string message) => _logger?.LogInformation($"{nameof(T)}: {message}");
+        public void Log(string message) => _logger?.LogInformation($"{_className}: {message}");
     }
 }
